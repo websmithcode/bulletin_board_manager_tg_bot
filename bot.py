bac37951c16a15aac5af4be7be879a9478da2ed2
twipz333 <dimago2001@gmail.com>
2022-09-15 19:31:40 +0300

    Минорные изменения - Обновлен .gitignore - Добавлено описание методов - Исправлено срабатывание хендлера на все сообщения

--- bot.py
+++ bot.py
@@ -1,28 +1,38 @@
+"""Точка запуска бота
+"""
 import os
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import BotCommand
+from telebot.util import content_type_media
 from utils.logger import log
 from handlers.group import on_message_received
 from handlers.private import  callback_query
 
 __TOKEN = os.environ.get('TOKEN')
-bot = AsyncTeleBot(__TOKEN, parse_mode='Markdown')
+bot = AsyncTeleBot('5546791980:AAEPW5wODZamrOp_jpxVlt6SNid9y4Dos0A', parse_mode='Markdown')
 
 
 def register_handlers():
-    bot.register_message_handler(callback=on_message_received, content_types=['text'], pass_bot=True)
-    bot.register_callback_query_handler(callback=callback_query, func=lambda call: True, pass_bot=True)
+    """Регистрация хендлеров бота
+    """
+    bot.register_message_handler(callback=on_message_received,
+                                content_types=content_type_media,
+                                pass_bot=True)
+    bot.register_callback_query_handler(callback=callback_query,
+                                        func=lambda call: True,
+                                        pass_bot=True)
 
 
 register_handlers()
-
+print(content_type_media)
 
 # async def set_commands():
 #     await bot.set_my_commands(commands=[BotCommand()])
-    
+
 
 def start_polling():
+    """Метод запуска получения обновлений"""
     log.info("Starting polling...")
     # asyncio.run(set_commands())
     asyncio.run(bot.polling(
--- handlers/group.py
+++ handlers/group.py
@@ -1,4 +1,3 @@
-import os
 """Модуль групповых хендлеров"""
 from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.async_telebot import AsyncTeleBot
@@ -8,10 +7,15 @@ from utils.database import AdminDatabase
 db = AdminDatabase()
 
 
-def create_markup():
+def create_markup() -> InlineKeyboardButton:
+    """Метод создающий разметку сообщения
+
+    Returns:
+        InlineKeyboardButton: Разметка сообщения
+    """
     message_check_markup = InlineKeyboardMarkup()
-    accept_button = InlineKeyboardButton('Accept', callback_data='accept')
-    decline_button = InlineKeyboardButton('Decline', callback_data='decline')
+    accept_button = InlineKeyboardButton('Принять', callback_data='accept')
+    decline_button = InlineKeyboardButton('Отклонить', callback_data='decline')
     message_check_markup.add(accept_button, decline_button)
     return message_check_markup
 
@@ -22,18 +26,49 @@ async def on_message_received(message: Message, bot: AsyncTeleBot):
     Args:
         message (Message): объект сообщения
         bot (AsyncTeleBot): объект бота
-    """    
-    if message.chat.type not in ('group', 'supergroup'): 
+    """
+    name = message.from_user.username if message.from_user.username else message.from_user.full_name
+    text = message.text if message.text else message.caption
+    message_type = message.content_message_type
+
+    if message.chat.message_type not in ('group', 'supergroup'):
         return
-    log.info('Received message: %s from %s', message.text, message.sender_chat.username, message.from_user.id)
-    text = message.text
-    text += f'\n\n[{message.from_user.username if message.from_user.username else message.from_user.full_name}](tg://user?id={message.from_user.id})'
+
+    log.info('Received message: %s from %s, %s', text, name, message.from_user.id)
+
+    if message_type in ('text', 'photo', 'video', 'document') and text:
+        text += f'\n\n[{name}](tg://user?id={message.from_user.id})'
+        params = {'reply_markup': create_markup()}
+
+        if message_type == 'text':
+            send = bot.send_message
+            params['text'] = text
+
+        elif message_type == 'photo':
+            send = bot.send_photo
+            params['caption'] = text
+            # возьмет только первое изображение
+            params['photo'] = message.json.get('photo')[0].get('file_id')
+            bot.send_photo(1,)
+
+        elif message_type == 'video':
+            # не сработает
+            send = bot.send_video
+            params['caption'] = text
+            params['video'] = message.video
+
+        else:
+            send = bot.send_document
+            params['document'] = message.document
+            params['caption'] = text
+
+        for admin in db.admins:
+            params['chat_id'] = admin.get('id')
+            log.debug(send)
+            await send(**params)
+
     await bot.delete_message(message.chat.id, message.id)
-    for admin in db.admins:
-        await bot.send_message(admin.get('id'), message.text, reply_markup=create_markup())
     #сохраняем сообщение
     #удаляем сообщение
     #отправляем админам
-
-
-
+    
