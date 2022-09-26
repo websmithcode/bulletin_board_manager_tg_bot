5bf01061f036792051f3eef0045560173dd658d6
twipz333 <dimago2001@gmail.com>
2022-09-27 00:22:42 +0300

    Hotfix 820dcc7fc021114e55849ff0fc63c51a8e7c72f1

--- handlers/admin_configs.py
+++ handlers/admin_configs.py
@@ -156,9 +156,9 @@ def get_send_procedure(message_type: str, bot: AsyncTeleBot) -> Callable: #pylin
 def string_builder(**kwargs):
     text = f"{' '.join([tag for tag in kwargs.pop('tags', [])])}\n"\
     f"\n{kwargs.pop('text')}\n\n"\
-    "\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"\
     'Если вас заинтересовало данное предложение напишите:\n'\
     f"[{kwargs.pop('username')}](tg://user?id={kwargs.pop('user_id')})\n\n"\
+    "\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"\
     f"{kwargs.pop('ps')}"
     
     return text
